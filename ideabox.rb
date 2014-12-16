9c3f4fd81fe76a2070def69500473296ba084685
Scott Crawford <scottcrawford03@gmail.com>
2014-12-16 13:56:32 -0700

    a lot of stuff, on ch. 8 now

--- ideabox.rb
+++ ideabox.rb
@@ -1,17 +1,35 @@
 require "./idea"
+require "./idea_store"
 
 class IdeaBoxApp < Sinatra::Base
+  set :method_override, true
+
   configure :development do
     register Sinatra::Reloader
   end
 
   get '/' do
-    erb :index
+    erb :index, locals: {ideas: IdeaStore.all}
   end
 
   post '/' do
-    idea = Idea.new
-    "Creating an IDEA!"
+    idea = IdeaStore.create(params[:idea])
+    redirect '/'
+  end
+
+  delete '/:id' do |id|
+    IdeaStore.delete(id.to_i)
+    redirect '/'
+  end
+
+  get '/:id/edit' do |id|
+    idea = IdeaStore.find(id.to_i)
+    erb :edit, locals: {id: id, idea: idea}
+  end
+
+  put '/:id' do |id|
+    IdeaStore.update(id.to_i, params[:idea])
+    redirect '/'
   end
 
   not_found do
