fdc8ac1d575881bd467089ad9d94d2b4c63b3985
Scott Crawford <scottcrawford03@gmail.com>
2014-12-16 11:38:26 -0700

    made error page and idea class

---
+++ config.ru
@@ -0,0 +1,6 @@
+require 'bundler'
+Bundler.require
+
+require './ideabox'
+
+run IdeaBoxApp
--- ideabox.rb
+++ ideabox.rb
@@ -1,3 +1,20 @@
-class IdeaBoxApp < Sinatra::Base
+require "./idea"
 
+class IdeaBoxApp < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    erb :index
+  end
+
+  post '/' do
+    idea = Idea.new
+    "Creating an IDEA!"
+  end
+
+  not_found do
+    erb :error
+  end
 end
