ee92ddc3c348b749adc585d7578897e65fde4fe8
Scott Crawford <scottcrawford03@gmail.com>
2014-12-17 10:50:29 -0700

    versions working

--- lib/idea_box/idea.rb
+++ lib/idea_box/idea.rb
@@ -6,16 +6,22 @@ class Idea
               :id,
               :tag,
               :version,
-              :history
+              :history,
+              :created_at,
+              :day_created,
+              :time_created
 
   def initialize(attributes)
-    @title       = attributes['title']
-    @description = attributes['description']
-    @rank        = attributes['rank'] || 0
-    @id          = attributes['id']
-    @tag         = attributes['tag']
-    @version     = attributes['version'] || 0
-    @history     = attributes['history'] || []
+    @title        = attributes['title']
+    @description  = attributes['description']
+    @rank         = attributes['rank'] || 0
+    @id           = attributes['id']
+    @tag          = attributes['tag']
+    @version      = attributes['version'] || 0
+    @history      = attributes['history'] || []
+    @created_at   = attributes['created_at'] || Time.new
+    @day_created  = created_at.strftime('%A')
+    @time_created = created_at.strftime('%l:%M %p')
   end
 
   def save
@@ -24,12 +30,15 @@ class Idea
 
   def to_h
     {
-      "title"       => title,
-      "description" => description,
-      "rank"        => rank,
-      "tag"         => tag,
-      "version"     => version,
-      "history"     => history
+      "title"        => title,
+      "description"  => description,
+      "rank"         => rank,
+      "tag"          => tag,
+      "version"      => version,
+      "created_at"   => created_at,
+      "day_created"  => day_created,
+      "time_created" => time_created,
+      "history"      => history
     }
   end
 
@@ -39,9 +48,10 @@ class Idea
 
   def update!(params, idea)
     @history << idea
-    @title = params["title"]
+    @created_at  = Time.new
+    @title       = params["title"]
     @description = params["description"]
-    @tag = params["tag"]
+    @tag         = params["tag"]
     @version += 1
   end
 
