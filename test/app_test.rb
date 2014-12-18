372f49edcd56bda8549ecc985302e7d7b676c4cc
Scott Crawford <scottcrawford03@gmail.com>
2014-12-18 13:46:14 -0700

    added more tests

--- test/app_test.rb
+++ test/app_test.rb
@@ -18,4 +18,26 @@ class IdeaBoxTest < Minitest::Test
     get '/'
     assert last_response.ok?
   end
+
+  def test_edit_route_works
+    get '/:id/edit'
+    assert last_response.ok?
+  end
+
+  def test_index_route_has_header
+    get '/'
+    html = Nokogiri::HTML(last_response.body)
+    assert html.css('h1').to_s.include?('IdeaCAGE')
+  end
+
+  def test_index_route_has_existing_ideas
+    get '/'
+    html = Nokogiri::HTML(last_response.body)
+    assert html.css('h3').to_s.include?('Existing')
+  end
+
+  def test
+    get '/'
+
+  end
 end
