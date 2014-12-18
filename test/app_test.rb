ef9c15ee9f0375457009db5e28f09476f2bb82d1
Scott Crawford <scottcrawford03@gmail.com>
2014-12-18 13:49:01 -0700

    added more tests

--- test/app_test.rb
+++ test/app_test.rb
@@ -36,8 +36,24 @@ class IdeaBoxTest < Minitest::Test
     assert html.css('h3').to_s.include?('Existing')
   end
 
-  def test
-    get '/'
+  def test_all_tags_works
+    get '/tags'
+    assert last_response.ok?
+  end
 
+  def test_tag_route_has_existing_ideas
+    get '/tags'
+    html = Nokogiri::HTML(last_response.body)
+    assert html.css('h1').to_s.include?('Tags')
+  end
+
+  def test_single_tags_works
+    get '/tags/:tag'
+    assert last_response.ok?
+  end
+
+  def test_idea_versions_works
+    get '/:id/versions'
+    assert last_response.ok?
   end
 end
